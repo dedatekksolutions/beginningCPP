197b67988d1d12048363db35cfec58cff72a8864
dedatekk <dedatekk@gmail.com>
2023-04-30 11:13:31 +1000

    prg1

---
+++ project1/favno.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int favno;
+
+    std::cout << "Put a number in bitch";
+    
+    std::cin >> favno;
+    
+    std::cout << "Balls mcginty:"<< std::endl; 
+  
+    std::cout << favno<< std::endl;
+    
+
+
+    return 0;
+}
