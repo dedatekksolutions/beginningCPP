a6260ef3cc1fad23ac8d665605dbcc5866794b16
dedatekk <dedatekk@gmail.com>
2023-05-07 21:01:38 +1000

    "46"

---
+++ project1/calculate-area.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+
+int room_width;
+int room_length;
+
+std::cout<< "Enter the width of the room";
+std::cin>>room_width;
+
+
+std::cout<< "Enter the length of the room";
+std::cin>>room_length;
+
+std::cout<<"The area of the room is " << room_width * room_length << " metres" << std::endl;
+
+    return 0;
+}
