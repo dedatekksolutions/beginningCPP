2ab3c0e1bfe5021a13917f6f770c3a532f23d372
dedatekk <dedatekk@gmail.com>
2023-05-16 04:12:54 +1000

    57 MD arrays

---
+++ MultiDimensionalArrays/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+
+//best practice for declaring MD arrays
+    int movie_rating[3][4]{
+        {0, 4, 3, 5},
+        {2, 3, 3, 5},
+        {1, 4, 4, 5}};
+
+    return 0;
+}
